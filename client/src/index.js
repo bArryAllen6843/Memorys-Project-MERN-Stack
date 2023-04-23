6dacc638d1162220f84db481266f5e76fc6535d7
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-04-23 17:01:58 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+import App from './App';
+import './index.css';
+
+// const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = configureStore({
+    reducer : reducers,
+})
+
+createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    
+);
